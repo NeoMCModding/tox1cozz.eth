f63ee36f79bfca3341619d3a755ce4d00559250b
Aizistral <admin@aizistral.com>
2024-05-22 02:11:54 +0200

    Use UTF-8 when reading localization file

--- src/main/java/com/aizistral/tox1cozz/config/Localization.java
+++ src/main/java/com/aizistral/tox1cozz/config/Localization.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -27,7 +28,7 @@ public class Localization {
         if (stream == null)
             throw new IOException("Localization file not found");
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream))) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
             String line = null;
 
             while ((line = reader.readLine()) != null) {
